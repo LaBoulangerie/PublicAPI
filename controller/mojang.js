bdef8d87f4ab4d7b1b2470d8fc1e43260818e8ee
Grooble <grooble.dierne@gmail.com>
2022-01-21 23:22:23 +0100

    Fixed CORS

--- controller/mojang.js
+++ controller/mojang.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch')
 
 async function mojang(req, res) {
-    console.log(req.url)
+    res.header('Access-Control-Allow-Origin', '*');
     return fetch(`https://api.mojang.com/${req.url.split('/mojang/')[1]}`).then((res) => res.json());
 }
 
