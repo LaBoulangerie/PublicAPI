52b173f82649d34868b13be755a0e3c58be37ba4
Grooble <grooble.dierne@gmail.com>
2022-01-21 23:45:49 +0100

    Removed now useless header

--- controller/mojang.js
+++ controller/mojang.js
@@ -1,7 +1,6 @@
 const fetch = require('node-fetch')
 
 async function mojang(req, res) {
-    res.header('Access-Control-Allow-Origin', '*');
     return fetch(`https://api.mojang.com/${req.url.split('/mojang/')[1]}`).then((res) => res.json());
 }
 
